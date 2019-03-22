c8d78e6526653e9f5819fa280d460b680c15cc44
Wattanit Hotrakool <wattanit@Wattanits-MacBook-Pro-Black.local>
2019-03-22 12:32:24 +0700

    Add navigations to header

--- src/components/header.js
+++ src/components/header.js
@@ -33,27 +33,45 @@ const Header = (props) => (
 
   <header className="header">
     <div className="headerMenu">
-      <div>
+      <Link
+        to="/"
+        className="headerMenuItem"
+      >
         {MenuItems(props.language).home}
-      </div>
-      <div>
+      </Link>
+      <Link
+        to="/about"
+        className="headerMenuItem"
+      >
         {MenuItems(props.language).about}
-      </div>
-      <div>
+      </Link>
+      <Link
+        to="/resources"
+        className="headerMenuItem"
+      >
         {MenuItems(props.language).resources}
-      </div>
+      </Link>
     </div>
     <img className="headerLogo" src={headerLogo}/>
     <div className="headerMenu">
-      <div>
+      <Link
+        to="/get-involved"
+        className="headerMenuItem"
+      >
         {MenuItems(props.language).involved}
-      </div>
-      <div>
+      </Link>
+      <Link
+        to="/blog"
+        className="headerMenuItem"
+      >
         {MenuItems(props.language).blog}
-      </div>
-      <div>
+      </Link>
+      <Link
+        to="/contact"
+        className="headerMenuItem"
+      >
         {MenuItems(props.language).contact}
-      </div>
+      </Link>
     </div>
     <button className="headerLanguageButton" onClick={()=>{
       if (props.language === "en"){
@@ -66,40 +84,5 @@ const Header = (props) => (
     </button>
   </header>
 )
-
-// <header
-//     style={{
-//       background: `rebeccapurple`,
-//       marginBottom: `1.45rem`,
-//     }}
-//   >
-//     <div
-//       style={{
-//         margin: `0 auto`,
-//         maxWidth: 960,
-//         padding: `1.45rem 1.0875rem`,
-//       }}
-//     >
-//       <h1 style={{ margin: 0 }}>
-//         <Link
-//           to="/"
-//           style={{
-//             color: `white`,
-//             textDecoration: `none`,
-//           }}
-//         >
-//           {siteTitle}
-//         </Link>
-//       </h1>
-//     </div>
-//   </header>
-
-// Header.propTypes = {
-//   siteTitle: PropTypes.string,
-// }
-
-// Header.defaultProps = {
-//   siteTitle: ``,
-// }
 
 export default Header
